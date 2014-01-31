0cb49551d1440141ceb1ced4824ee76e3365db27
Eric <eri5mok@gmail.com>
2014-01-31 13:20:30 -0500

    Token now expires within 2 hours (on the client).

--- blog/static/blog/dist/app/main.js
+++ blog/static/blog/dist/app/main.js
@@ -155,7 +155,9 @@ angular.module('Security', ['Urls'])
 	return {
 		loginToken: null,
 		username: null,
-
+		tokenDate: 0,
+		TOKEN_LIFE_TIME: (2 * 60 * 60 * 1000), // 2 hours
+		
 		login: function(username, password) {
 
 			var self = this;
@@ -175,10 +177,15 @@ angular.module('Security', ['Urls'])
 			});
 
 			promise.success(function (data, status, headers, config) {
+
 				$window.sessionStorage.setItem("token", data.token);
 				$window.sessionStorage.setItem("username", username);
-				self.loginToken = data.token;
+				
+				self.tokenDate = (new Date()).getTime();
+				$window.sessionStorage.setItem("token_date", self.tokenDate);
 
+				self.loginToken = data.token;
+				self.username = username;
 				// return $http.get(urls.users + "/" + username).then(fnugction(response) {
 				// 	self.user = response.data;
 				// 	console.log("logged in");
@@ -186,15 +193,26 @@ angular.module('Security', ['Urls'])
 			});
 
 			promise.error(function(data, status, headers, config) {
+
 				$window.sessionStorage.removeItem("token");
 				self.loginToken = null;
 				self.username = null;
+				self.tokenDate = 0;
 			});
 
 			return promise;
 		},
 		isLoggedIn: function() {
-			return this.loginToken != null;
+			// On login query, check if token still valid, if not, reset everything
+			var lifeTime = this.TOKEN_LIFE_TIME;
+
+			if ( (new Date()).getTime() > this.tokenDate + lifeTime ) {
+				this.loginToken = null;
+				this.username = null;
+				this.tokenDate = 0;
+				return false;
+			}
+			return this.loginToken !== null;
 		},
 
 		logout: function() {
@@ -202,11 +220,15 @@ angular.module('Security', ['Urls'])
 			var self = this;
 
 			$window.sessionStorage.removeItem("token");
+			$window.sessionStorage.removeItem("token_date");
 			$window.sessionStorage.removeItem("username");
 
+			// Store the token in a private variable so it doesn't leak
 			var tempLoginToken = self.loginToken;
 			
+			// Reset all the things
 			self.loginToken = null;
+			self.tokenDate = 0;
 			self.username = null;
 
 			return $http({
@@ -259,6 +281,7 @@ angular.module('Security', ['Urls'])
 
 .run(function($window, auth) {
 	auth.loginToken = $window.sessionStorage.getItem("token");
+	auth.tokenDate = $window.sessionStorage.getItem("token_date");
 	auth.username = $window.sessionStorage.getItem("username");
 });
 
