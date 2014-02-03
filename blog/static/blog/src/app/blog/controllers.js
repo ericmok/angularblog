540218a816294b15ac50d24d63781b6bab4f86b6
Eric <eri5mok@gmail.com>
2014-02-03 16:21:12 -0500

    Working on blog branch.

--- blog/static/blog/src/app/blog/controllers.js
+++ blog/static/blog/src/app/blog/controllers.js
@@ -1,5 +1,15 @@
 angular.module('main')
 
-.controller('BlogCtrl', function($scope, $stateParams) {
+.controller('BlogCtrl', function($scope, $stateParams, $http, urls) {
+	
 	console.log($stateParams.blogId);
+	$scope.blogId = $stateParams.blogId;
+	$scope.blog = null;
+
+	$http({
+		url: [urls.blogs, '/', $scope.blogId].join(''),
+		method: 'GET'
+	}).then(function(response) {
+		$scope.blog = angular.toJson( response.data );
+	});
 });
