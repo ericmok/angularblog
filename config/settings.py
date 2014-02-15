adf1463d719f0b0beda091e0c91a28cae25b18dc
Eric <eri5mok@gmail.com>
2014-02-14 21:44:18 -0500

    Commented out roundtripdelay

--- config/settings.py
+++ config/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'blog.delay.RoundTripDelay',
+    #'blog.delay.RoundTripDelay',
 )
 
 ROOT_URLCONF = 'config.urls'
