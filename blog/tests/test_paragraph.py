82258a56fc42594948b81949eebc3ba02203888e
Eric <eri5mok@gmail.com>
2014-02-16 21:36:39 -0500

    Refactored test files so that they now work with the new URL root

--- blog/tests/test_paragraph.py
+++ blog/tests/test_paragraph.py
@@ -15,30 +15,7 @@ from blog.models import *
 import warnings
 import pdb
 
-ROOT_URL = 'http://localhost:8000/blog'
-
-AUTH_URL = ROOT_URL + '/api-tokens'
-
-SIGNIN_URL_FRAG = '/blog/sign-in'
-SIGNIN_URL = ROOT_URL + '/sign-in'
-USERS_URL = ROOT_URL + '/api/users'
-BLOGS_URL = ROOT_URL + '/api/blogs'
-POSTS_URL = ROOT_URL + '/api/posts'
-PARAGRAPH_URL = ROOT_URL + '/api/paragraphs'
-SENTENCES_URL = ROOT_URL + '/api/sentences'
-
-TEST_USER_ALICE = {
-	"username": "alice",
-	"password": "testtest"
-}
-
-TEST_USER_BOBBY = {
-	"username": "bobby",
-	"password": "testtest"
-}
-
-TEST_USER_ALICE_JSON = json.dumps(TEST_USER_ALICE)
-TEST_USER_BOBBY_JSON = json.dumps(TEST_USER_BOBBY)
+from config import *
 
 class ParagraphGetEndpoint(TestCase):
 	fixtures = ['nice_fixture3.json']
